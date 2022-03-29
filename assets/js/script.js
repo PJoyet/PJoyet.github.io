2c8fa29a0ff21f5a8ff1d35eff63724ce522cb69
PJoyet <pierre.joyet@yahoo.fr>
2022-03-29 20:39:44 +0200

    newdesign

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+
+var container = document.getElementById('container');
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+var scrollArea = 1000 - windowHeight;
+var square1 = document.getElementsByClassName('square')[0];
+var square2 = document.getElementsByClassName('square')[1];
+
+square1.style.right = 600 + 'px';
+
+// update position of square 1 and square 2 when scroll event fires.
+window.addEventListener('scroll', function() {
+  var scrollTop = window.pageYOffset || window.scrollTop;
+  var scrollPercent = scrollTop/scrollArea || 0;
+  
+  square1.style.right = 600 - scrollPercent*window.innerWidth*0.6 + 'px';
+});

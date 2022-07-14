9aac81fac566cc64b12c7d7ece494f593d1beab0
PJoyet <pierre.joyet@yahoo.fr>
2022-07-14 10:58:54 +0200

    commit du 17 juillet 2022

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
-
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 //var container = document.getElementById('container');
 //var windowHeight = window.innerHeight;
